54d58b320950435f089b874b292af8d67d6d2d0b
Apple <lzl05tx@163.com>
2022-09-04 18:53:44 +0800

    登录逻辑修改

--- src/main/login/login.component.ts
+++ src/main/login/login.component.ts
@@ -1,7 +1,7 @@
 import { UntypedFormGroup } from '@angular/forms';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { AuthService } from '../../services/auth.service';
+import { AuthService, User } from '../../services/auth.service';
 // import { environment } from '../../environments/environment';
 import { UntypedFormBuilder } from '@angular/forms';
 import { XMessageService } from '@ng-nest/ui/message';
@@ -9,6 +9,12 @@ import cloudbase from "@cloudbase/js-sdk";
 const app = cloudbase.init({
   env: "reward-wall-sys-3g10xtceb7153f73"
 });
+
+const auth = app
+  .auth({
+    persistence: 'local'
+  })
+const db = app.database();
 
 @Component({
   selector: 'app-login',
@@ -42,44 +48,49 @@ export class LoginComponent implements OnInit {
       let user = this.userForm.value;
       if (user.account && user.password) {
         this.loading = true;
-        app
-          .auth({
-            persistence: 'local'
-          }).signInWithEmailAndPassword(user.account, user.password).then((loginState) => {
-            // 邮箱密码登录成功
-            console.log(loginState);
-            this.loading = false;
-            if (loginState.user) {//登录成功
-              this.message.success('登录成功!');
-              this.authService.loginTest(user).subscribe(
-                () => {
-                  if (this.authService.isLoggedIn) {
-                    // let redirect = this.authService.redirectUrl ? this.authService.redirectUrl : `/${environment.layout}`;
-                    // this.router.navigate([redirect]);
-                    this.router.navigate(['/index']);
+
+
+
+        auth.signUpWithEmailAndPassword(user.account, user.password)
+          .then(() => {
+            // 发送验证邮件成功
+            this.message.warning('账户激活邮件已发送到您的邮箱!');
+          }).catch((error) => {
+            console.log("错误=======" + error)
+            if ((error+"").includes("mail user exist")) {
+              auth.signInWithEmailAndPassword(user.account, user.password).then((loginState) => {
+                  // 邮箱密码登录成功
+                  console.log(loginState);
+                  this.loading = false;
+                  if (loginState.user) {//登录成功
+                    db.collection("Users")
+                      .where({
+                        Email: user.account
+                      })
+                      .get()
+                      .then((res) => {
+                        console.log(res.data);
+                        if (res.data) {
+                          if (res.data.length > 0) {//本地库中有此用户
+                            this.loginSkip(user);
+                          } else {//本地库中没有此用户
+                            this.message.warning("请联系管理员为您添加账号！");
+                          }
+                        }
+                      });
+
+
+                    this.message.success('登录成功!');
+
                   }
-                },
-                () => {
+                })
+                .catch((error) => {
                   this.loading = false;
-                },
-                () => {
-                  this.loading = false;
-                }
-              );
+                  console.log("登录失败=========" + error);
+                  this.message.warning('登录失败，请联系管理员解决!');
+                });
             }
-          })
-          .catch((error) => {
-            this.loading = false;
-            console.log("登录失败=========" + error);
-            this.message.warning('登录失败，请联系管理员解决!');
           });
-
-
-        // .signUpWithEmailAndPassword(user.account, user.password)
-        // .then(() => {
-        //   // 发送验证邮件成功
-        //   this.message.warning('账户激活邮件已发送到您的邮箱!');
-        // });
 
 
       } else {
@@ -87,5 +98,24 @@ export class LoginComponent implements OnInit {
         this.message.warning('用户名或密码不能为空！');
       }
     }
+  }
+
+
+  loginSkip(user: User) {
+    this.authService.loginTest(user).subscribe(
+      () => {
+        if (this.authService.isLoggedIn) {
+          // let redirect = this.authService.redirectUrl ? this.authService.redirectUrl : `/${environment.layout}`;
+          // this.router.navigate([redirect]);
+          this.router.navigate(['/index']);
+        }
+      },
+      () => {
+        this.loading = false;
+      },
+      () => {
+        this.loading = false;
+      }
+    );
   }
 }
